5fe935f3eab30b31905af44f7d2deb2c9337f3d4
Shaikh Saad <saadshaikh98765@icloud.com>
2026-04-07 18:06:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,23 @@ import streamlit as st
 import pickle
 import pandas as pd
 
+st.markdown(
+    """
+    <style>
+    h1 {
+        color: #1F2937;
+        text-align: center;
+    }
+    .stButton>button {
+        background-color: #2563EB;
+        color: white;
+        border-radius: 8px;
+        font-weight: 500;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
 teams = ['Chennai Super Kings',
  'Delhi Capitals',
  'Gujarat Titans',
@@ -22,7 +39,13 @@ cities = ['Bangalore', 'Chandigarh', 'Delhi', 'Mumbai', 'Kolkata', 'Jaipur',
        'Navi Mumbai', 'Lucknow', 'Guwahati', 'Mohali']
 
 pipe = pickle.load(open('pipe.pkl','rb'))
-st.title('IPL Win Predictor')
+col_logo, col_title = st.columns([1, 4])
+
+with col_logo:
+    st.image('logo.png', width="stretch")
+
+with col_title:
+    st.title('IPL Win Predictor')
 
 col1, col2 = st.columns(2)
 
@@ -41,12 +64,23 @@ target = st.number_input('Target', step=1, format="%d")
 
 col3,col4,col5 = st.columns(3)
 
-with col3:
-    score = st.number_input('Score', step=1, format="%d")
+
+
 with col4:
     overs = st.number_input('Overs completed', step=1, format="%d", min_value=0, max_value=20)
 with col5:
-    wickets = st.number_input('Wickets out', step=1, format="%d", max_value=10)
+    wickets = st.number_input('Wickets out', step=1, format="%d", max_value=10, min_value=0)
+
+# Convert overs to balls (correct cricket logic)
+overs_int = int(overs)
+balls_in_over = int((overs - overs_int) * 10)
+total_balls = overs_int * 6 + balls_in_over
+# Max possible score based on overs
+max_score = total_balls * 6
+
+with col3:
+    score = st.number_input('Score', step=1, format="%d", min_value=0, max_value=max_score if total_balls > 0 else 0)
+
 if st.button('Predict Probability'):
 
     if overs == 0:
